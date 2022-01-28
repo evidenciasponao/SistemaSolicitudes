d576af20f786f1e712374e4d1387f55697e04741
evidenciasponao <evidenciasponao@gmail.com>
2022-01-27 23:56:19 -0500

    Consultando datos de la tabla registros

--- app.py
+++ app.py
@@ -19,10 +19,14 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql ="INSERT INTO `registro` (`id`, `cedula`, `lugarExpedicion`, `nombres`, `apellidos`, `telefono`, `email`, `empresaLaboro`, `cargo`, `fechaInicio`, `fechaRetiro`, `fechaNacimiento`, `fondoPension`, `foto`) VALUES (NULL, '1075234987', 'neiva', 'anderson alexis', 'montenegro melo', '3214521011', 'adn@gmail.com', 'idesac', 'auxiliar administrativo', '01 de enero 1985', '30 de diciembre 1985', '01 de marzo 1989', 'porvenir', 'foto.jpg');"
+    sql ="SELECT * FROM `registro`;"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
+
+    registro=cursor.fetchall()
+    print(registro)
+
     conn.commit()
 
     return render_template('registros/index.html')
